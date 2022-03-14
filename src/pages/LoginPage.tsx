0a7ef3458bf957b9b355d253c4b3b58a31d0393e
Canh-Kiet LE <kiet@makeit-studio.com>
2022-03-14 15:57:16 +0100

    feat(feature_1): login error handling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { useMutation } from 'react-query';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { auth } from '../lib/apiClient';
@@ -7,6 +9,9 @@ export const LoginPage = () => {
   const { setToken } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+  const [displayErrorMessage, setDisplayErrorMessage] = useState(false);
+
   const { mutate: login } = useMutation(auth, {
     onSuccess: ({ session_token }) => {
       setToken(session_token);
@@ -14,6 +19,17 @@ export const LoginPage = () => {
       const from = (location.state as any)?.from?.pathname || '/';
 
       navigate(from, { replace: true });
+    },
+    onError: (error) => {
+      if (!axios.isAxiosError(error) || error.response?.status === 503) {
+        setErrorMessage('An error occured');
+      } else if (error.response?.status === 404) {
+        setErrorMessage('Incorrect username or password');
+      } else if (error.response?.status === 403) {
+        setErrorMessage('User already logged in');
+      }
+
+      setDisplayErrorMessage(true);
     },
   });
 
@@ -30,6 +46,26 @@ export const LoginPage = () => {
   return (
     <div className="chi-main__content">
       <div className="chi-grid">
+        {displayErrorMessage && (
+          <div className="chi-col -w--4 -o--4 -mb--2">
+            <div className="chi-alert -danger -banner -close" role="alert">
+              <i className="chi-alert__icon chi-icon icon-circle-warning" aria-hidden="true"></i>
+              <div className="chi-alert__content">
+                <p className="chi-alert__text">{errorMessage}</p>
+              </div>
+              <button
+                className="chi-alert__close-button chi-button -icon -close"
+                aria-label="Close"
+                onClick={() => setDisplayErrorMessage(false)}
+              >
+                <div className="chi-button__content">
+                  <i className="chi-icon icon-x" aria-hidden="true"></i>
+                </div>
+              </button>
+            </div>
+          </div>
+        )}
+
         <div className="chi-col -w--4 -o--4">
           <div className="chi-card">
             <div className="chi-card__content">
@@ -38,13 +74,19 @@ export const LoginPage = () => {
                   <label className="chi-label" htmlFor="username">
                     Username
                   </label>
-                  <input type="text" className="chi-input" name="username" id="username" />
+                  <input type="text" className="chi-input" name="username" id="username" required />
                 </div>
                 <div className="chi-form__item -my--3">
                   <label className="chi-label" htmlFor="password">
                     Password
                   </label>
-                  <input type="password" className="chi-input" name="password" id="password" />
+                  <input
+                    type="password"
+                    className="chi-input"
+                    name="password"
+                    id="password"
+                    required
+                  />
                 </div>
                 <div className="-my--3 -d--flex -justify-content--end">
                   <input className="chi-button -primary" type="submit" value="Log in" />
